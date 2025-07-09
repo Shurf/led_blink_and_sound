1dae4005f422cfdf32f12d6d7c9e21672efbfceb
Daniil Yavno <daniil.yavno@veeam.com>
2025-07-10 00:29:53 +0200

    latest updates

--- src/HttpControl.h
+++ src/HttpControl.h
@@ -1,9 +1,13 @@
+#pragma once
+
 #include "Arduino.h"
 #include "HTTPClient.h"
 #include "ArduinoJson.h"
 
 #define MAX_OUTPUT_VALUE 255
 #define PROFILE_NAME "LED1"
+#define MAX_TRIES_COUNT 5
+#define SLEEP_INTERVAL 1000
 
 struct Arguments {
     String profileName;
@@ -26,10 +30,15 @@ class HttpControl
 {    
 public:
 
+    HttpControl();
     bool getLedProfileParameters(String& profileName, int& red, int& green, int& blue, int& percentage);
     Arguments getLedProfileFullParameters();
     
 private:
 
-    String getCurrentLedProfileUrl = "http://led.haven/neon_led_control/led_profiles/get_by_name?led_profile_name=" PROFILE_NAME;
+    String baseMacString;
+    //String getCurrentLedProfileUrl = "http://led.haven/neon_led_control/led_profiles/get_by_name?led_profile_name=" PROFILE_NAME;
+    String getCurrentLedProfileUrl = "http://led.haven/neon_led_control/led_profiles/current";
+    //String getCurrentLedProfileUrl = "http://192.168.0.120:8000/neon_led_control/led_profiles/current";
+    void readMacAddress();
 };
